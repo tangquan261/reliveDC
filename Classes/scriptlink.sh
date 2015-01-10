eedbb894e03d5747f4e13c1dca99b78d2d842d15
tangquan <tangquan@hoolai.com>
2015-01-10 13:23:55 +0800

    no message

---
+++ Classes/scriptlink.sh
@@ -0,0 +1,7 @@
+#/bash/sh
+
+rm -rf /Users/tq/Library/Developer/Xcode/DerivedData/DivineComedy-gnsrsmogdkyscidtotwfaukpmnju/Build/Products/Debug/DivineComedyMac.app/Contents/Resources/scripts
+
+ln -s /Users/tq/work/shenqu/Code/Classes/scripts  /Users/tq/Library/Developer/Xcode/DerivedData/DivineComedy-gnsrsmogdkyscidtotwfaukpmnju/Build/Products/Debug/DivineComedyMac.app/Contents/Resources/scripts
+
+
